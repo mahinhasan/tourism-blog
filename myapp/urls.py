952596064359f2a1b1141d73e48b545fd9a429f7
mahinhasan <mahinhasanaiub@gmail.com>
2021-11-04 13:01:16 +0600

    changes in regi

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,12 +6,15 @@ from .import views
 urlpatterns = [
     
     path('',PostLIst.as_view(),name='home'),
-    path('rapist/<int:pk>',RapistDetail.as_view(),name='rapist-detail'),
-    path('addrapist',AddRppist.as_view(),name='addrapist'),
+    path('blog-post /<int:pk>',RapistDetail.as_view(),name='rapist-detail'),
+    path('create_post',AddRppist.as_view(),name='addrapist'),
     path('update/<int:pk>',UpdateRapist.as_view(),name='update'),
     path('delete/<int:pk>/remove',DeleteRapist.as_view(),name='delete'),
     path('register',UserRegister.as_view(),name = 'register'),
-    path('visitor',views.index,name='visitor')
+    path('visitor',views.index,name='visitor'),
+    path('users',views.showthis,name='users'),
+    path('blogs',views.allBlog,name='blogs'),
+    path('search',views.search,name='search')
 
 
 
--- myapp/views.py
+++ myapp/views.py
@@ -5,6 +5,10 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.forms import UserCreationForm
 from .forms import RapistForm
 from django.urls import reverse_lazy
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+
+
 
 from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 
@@ -16,9 +20,14 @@ class RapistDetail(DetailView):
 class PostLIst(ListView):
     model = Tourism
     form_class = RapistForm
+    
     template_name = 'myapp/index.html'
     ordering = ['-id']
     paginate_by = 4
+
+   
+
+            
 
 
 class AddRppist(CreateView):
@@ -34,7 +43,7 @@ class UpdateRapist(UpdateView):
     fields = '__all__'
 
 
-class DeleteRapist(DetailView):
+class DeleteRapist(DeleteView):
     model = Tourism
     template_name = 'myapp/delete_rapist.html'
     success_url = reverse_lazy('home')
@@ -53,13 +62,36 @@ class UserRegister(generic.CreateView):
 
 
 def index(request):
-
+    users = User.objects.all()
     num_visits = request.session.get('num_visits',0)
     request.session['num_visits'] = num_visits+1
-    context = {'visitor':num_visits}
-    return render(request,'myapp/home.html',context=context)
+    context = {'visitor':num_visits,'users':users}
+    return render(request,'myapp/index.html',context=context)
 
+from django.shortcuts import render
+from django.contrib.auth import get_user_model
 
+def showthis(request):
+
+    all_users= get_user_model().objects.all()
+    object_lists = Tourism.objects.all()
+    
+    context= {'allusers': all_users,'object_list':object_lists}
+        
+    return render(request, 'myapp/index.html', context)
+
+def allBlog(request):
+
+    blogs = Tourism.objects.all()
+    context = {'blogs':blogs}
+    return  render(request,'myapp/blogs.html',context)
+
+def search(request):
+    if request.method=='get':
+        sr = request.GET.get('s')
+        result = Tourism.objects.filter(place__icontains=sr)
+        context = {'resutl':result}
+        return render(request,'myapp/search.html',context)
 
 
 
